40a548dd8a48c3806255453bf19ed05cd3025372
Daniel Niedergesäß <daniel@caseable.com>
2014-05-06 12:34:33 +0200

    added initial mappings model

--- app/code/community/Dng/Elasticgento/Model/Abstract/Mappings.php
+++ app/code/community/Dng/Elasticgento/Model/Abstract/Mappings.php
@@ -8,7 +8,7 @@
  * @author    Daniel Niedergesäß <daniel.niedergesaess@gmail.com>
  * @version   1.0.0
  */
-class Dng_Elasticgento_Model_Abstract_Mappings
+abstract class Dng_Elasticgento_Model_Abstract_Mappings
 {
     /**
      * Current scope (store Id)
@@ -18,11 +18,53 @@ class Dng_Elasticgento_Model_Abstract_Mappings
     protected $_storeId;
 
     /**
+     * Entity object to define collection's attributes
+     *
+     * @var Mage_Eav_Model_Entity_Abstract
+     */
+    protected $_entity = null;
+
+    /**
+     * Eav Entity Type Id
+     *
+     * @var int
+     */
+    protected $_entityTypeId;
+
+    /**
+     * array with Attribute codes for index
+     *
+     * @var array
+     */
+    protected $_attributeCodes;
+
+    /**
+     * Attribute objects
+     *
+     * @var array
+     */
+    protected $_attributes = null;
+
+    /**
+     * index templates for dynamic fields
+     *
+     * @var array
+     */
+    protected $_dynamicTemplates = array();
+
+    /**
      * already created mappings as array cache by store id
      *
      * @var array
      */
     protected $_MappingsCache = array();
+
+    /**
+     * Retrieve entity type
+     *
+     * @return string
+     */
+    abstract function getEntityType();
 
     /**
      * Set store scope
@@ -72,5 +114,90 @@ class Dng_Elasticgento_Model_Abstract_Mappings
     public function getDefaultStoreId()
     {
         return Mage_Catalog_Model_Abstract::DEFAULT_STORE_ID;
+    }
+
+    /**
+     * Get mapping entity object
+     *
+     * @return Mage_Eav_Model_Entity_Abstract
+     */
+    public function getEntity()
+    {
+        if (null === $this->_entity) {
+            $this->_entity = Mage::getModel('eav/entity')->setType($this->getEntityType());
+        }
+        return $this->_entity;
+    }
+
+    /**
+     * Retrieve Catalog Entity Type Id
+     *
+     * @return int
+     */
+    public function getEntityTypeId()
+    {
+        if ($this->_entityTypeId === null) {
+            $this->_entityTypeId = $this->getEntity()->getTypeId();
+        }
+        return $this->_entityTypeId;
+    }
+
+    /**
+     * Retrieve attribute objects for flat
+     *
+     * @return array
+     */
+    public function getAttributes()
+    {
+        if ($this->_attributes === null) {
+            $this->_attributes = array();
+            $attributeCodes = $this->getAttributeCodes();
+            $entity = Mage::getSingleton('eav/config')->getEntityType($this->getEntityType())->getEntity();
+            foreach ($attributeCodes as $attributeCode) {
+                $attribute = Mage::getSingleton('eav/config')
+                    ->getAttribute($this->getEntityType(), $attributeCode)->setEntity($entity);
+                try {
+                    // check if exists source and backend model.
+                    // To prevent exception when some module was disabled
+                    $attribute->usesSource() && $attribute->getSource();
+                    $attribute->getBackend();
+                    $this->_attributes[$attributeCode] = $attribute;
+                } catch (Exception $e) {
+                    Mage::logException($e);
+                }
+            }
+        }
+
+        return $this->_attributes;
+    }
+
+    /**
+     * get list of all attribute codes
+     * @todo maybe add some filters to reduce amount of data
+     */
+    public function getAttributeCodes()
+    {
+        if ($this->_attributeCodes === null) {
+            /** @var Mage_Core_Model_Resource $resource */
+            $resource = Mage::getSingleton('core/resource');
+            $select = $resource->getConnection('core_read')->select();
+
+            $this->_attributeCodes = array();
+            $select = $select
+                ->from(array('main_table' => $resource->getTableName('eav/attribute')))
+                ->join(
+                    array('additional_table' => $resource->getTableName('catalog/eav_attribute')),
+                    'additional_table.attribute_id = main_table.attribute_id'
+                )
+                ->where('main_table.entity_type_id = :entity_type_id');
+            $result = $resource->getConnection('core_read')->fetchAll($select, array('entity_type_id' => $this->getEntityTypeId()));
+            Mage::getSingleton('eav/config')
+                ->importAttributesData($this->getEntityType(), $result);
+            foreach ($result as $data) {
+                $this->_attributeCodes[$data['attribute_id']] = $data['attribute_code'];
+            }
+            unset($result);
+        }
+        return $this->_attributeCodes;
     }
 }
--- app/code/community/Dng/Elasticgento/Model/Catalog/Product/Elasticgento/Mappings.php
+++ app/code/community/Dng/Elasticgento/Model/Catalog/Product/Elasticgento/Mappings.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Catalog Product Elasticgento Index Settings
+ * Catalog Product Elasticgento Index Mappings
  *
  * @category  Dng
  * @package   Dng_Elasticgento
@@ -10,5 +10,57 @@
  */
 class Dng_Elasticgento_Model_Catalog_Product_Elasticgento_Mappings extends Dng_Elasticgento_Model_Abstract_Mappings
 {
+    /**
+     * index templates for dynamic fields
+     *
+     * @var array
+     */
+    protected $_dynamicTemplates = array(
+        array(
+            'template_price_index' =>
+                array(
+                    "path_match" => 'price_index.price_customer_group_*',
+                    'match' => 'price_customer_group_*',
+                    'mapping' => array(
+                        'type' => 'object',
+                        'properties' => array(
+                            'price' => array('type' => 'double'),
+                            'min_price' => array('type' => 'double'),
+                            'final_price' => array('type' => 'double'),
+                            'max_price' => array('type' => 'double'),
+                            'tier_price' => array('type' => 'double'),
+                            'group_price' => array('type' => 'double')
+                        )
+                    )
+                ),
+        ),
+        array('template_category_sort' =>
+            array(
+                "path_match" => "category_sort.category_*",
+                'match' => 'category_sort',
+                'mapping' => array(
+                    'type' => 'integer'
+                )
+            )
+        ),
+        array('template_link_types' =>
+            array(
+                "path_match" => "product_link.*",
+                'match' => '*',
+                'mapping' => array(
+                    'type' => 'integer'
+                )
+            )
+        )
+    );
 
+    /**
+     * Retrieve entity type
+     *
+     * @return string
+     */
+    public function getEntityType()
+    {
+        return Mage_Catalog_Model_Product::ENTITY;
+    }
 }
